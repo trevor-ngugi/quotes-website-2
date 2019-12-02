c68236641f8a46e9e2a52ee6f1524d31cd5952cd
trevor ngugi <ngugitrevor@gmail.com>
2019-12-02 04:50:53 +0300

    added author name function

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription:boolean;
-    constructor(public id:number,public define:string,public details:string,public postDate:Date)
+    constructor(public id:number,public define:string,public details:string,public author:string, public postDate:Date)
     {this.showDescription=false;
     }
 
