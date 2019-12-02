7c45bbfa174ec9fe6a67713fdb094dceb0b47374
trevor ngugi <ngugitrevor@gmail.com>
2019-12-02 05:25:55 +0300

    change author details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,16 +8,16 @@ import{Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote(1,'work hard play hard','trevor','trevor',new Date(2019,10,30)),
-    new Quote(2 ,'When you can’t find the sunshine, be the sunshine','ngugi', 'trevor',new Date(2019,2,14)),
-    new Quote(3 ,'The grass is greener where you water it ','john', 'trevor',new Date(2019,2,14)),
-    new Quote(4 ,'Wherever life plants you, bloom with grace ','phillip', 'trevor',new Date(2019,2,15)),
-    new Quote(5 ,'Little by little, day by day, what is mean for you WILL find its way ','messi', 'trevor',new Date(2019,9,10)),
-    new Quote(6 ,' am in charge of how I feel and today I am choosing happiness ','crystal', 'trevor',new Date(2019,7,7)),
-    new Quote(7 ,'Learn from yesterday, live for today, hope for tomorrow ','jenkins', 'trevor',new Date(2019,1,15)),
-    new Quote(8 ,'Take time to do what makes your soul happy ','wakiritho', 'trevor',new Date(2019,6,17)),
-    new Quote(9 ,'Be so happy that when others look at you, they become happy too ','cole', 'trevor',new Date(2019,2,20)),
-    new Quote(10 ,'Believe. Act as if. Live like you already have it. It’s coming ','huncho', 'trevor',new Date(2019,3,14)),
+    new Quote(1,'work hard play hard','trevor','the johnson',new Date(2019,10,30)),
+    new Quote(2 ,'When you can’t find the sunshine, be the sunshine','ngugi', 'oscar wilde',new Date(2019,2,14)),
+    new Quote(3 ,'The grass is greener where you water it ','john', 'maryline monroe',new Date(2019,2,14)),
+    new Quote(4 ,'Wherever life plants you, bloom with grace ','phillip', 'frank zappa',new Date(2019,2,15)),
+    new Quote(5 ,'Little by little, day by day, what is mean for you WILL find its way ','messi', 'dr seuss',new Date(2019,9,10)),
+    new Quote(6 ,' am in charge of how I feel and today I am choosing happiness ','crystal', 'mae west',new Date(2019,7,7)),
+    new Quote(7 ,'Learn from yesterday, live for today, hope for tomorrow ','jenkins', 'mahatma gandhi',new Date(2019,1,15)),
+    new Quote(8 ,'Take time to do what makes your soul happy ','wakiritho', 'albert camus',new Date(2019,6,17)),
+    new Quote(9 ,'Be so happy that when others look at you, they become happy too ','cole', 'mark twain',new Date(2019,2,20)),
+    new Quote(10 ,'Believe. Act as if. Live like you already have it. It’s coming ','huncho', 'andre gide',new Date(2019,3,14)),
     
   ]
 
