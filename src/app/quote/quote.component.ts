2137c4916f248d6ce5e75a2a0b9777e5251ca753
trevor ngugi <ngugitrevor@gmail.com>
2019-12-02 04:19:51 +0300

    added new quotes description

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,16 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
     new Quote(1,'work hard play hard','trevor',new Date(2019,10,30)),
-    new Quote(2 ,'african warriors ','ngugi',new Date(2019,2,14))
+    new Quote(2 ,'When you can’t find the sunshine, be the sunshine','ngugi',new Date(2019,2,14)),
+    new Quote(3 ,'The grass is greener where you water it ','ngugi',new Date(2019,2,14)),
+    new Quote(4 ,'Wherever life plants you, bloom with grace ','ngugi',new Date(2019,2,14)),
+    new Quote(5 ,'Little by little, day by day, what is mean for you WILL find its way ','ngugi',new Date(2019,2,14)),
+    new Quote(6 ,' am in charge of how I feel and today I am choosing happiness ','ngugi',new Date(2019,2,14)),
+    new Quote(7 ,'Learn from yesterday, live for today, hope for tomorrow ','ngugi',new Date(2019,2,14)),
+    new Quote(8 ,'Take time to do what makes your soul happy ','ngugi',new Date(2019,2,14)),
+    new Quote(9 ,'Be so happy that when others look at you, they become happy too ','ngugi',new Date(2019,2,14)),
+    new Quote(10 ,'Believe. Act as if. Live like you already have it. It’s coming ','ngugi',new Date(2019,2,14)),
+    
   ]
 
   addNewQuote(quote){
